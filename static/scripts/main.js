41d58a494cc7449de2d4c0c0fcb7d98387a18483
miroshko <miroshko@gmail.com>
2013-04-02 15:03:45 +0300

    adding second module

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,15 +1,27 @@
 var domain = 'http://lab.Innovecs.local/';
-var Service = function(){};
+var Service = function( name, options ){
+  options = options || {}
+  this.name = name
+  this.url = options.url
+};
 var services = [];
 
-var hello_world = new Service();
+var hello_world = new Service( 'hello_world' );
 hello_world.url = 'hello_world';
 
+var pizzr = new Service( 'pizzr', {url: 'pizzr'} );
+
 services.push(hello_world);
+services.push(pizzr)
 
 var json;
-$.each(services, function(index, service_id){
-    json = $.getJSON(domain + service_id.url, function(json){
-        $('#first_service').html(json.data);
-    });
+$.each(services, function(index, service){
+	  var service_elem = $('#' + service.name)
+	  if (!service_elem.length) {
+	      service_elem = $('<div>', {id: service.name}).appendTo('body')
+	  }
+
+    json = $.getJSON(domain + service.url), function(json){
+        service_elem.html(json.data);
+    }
 });
